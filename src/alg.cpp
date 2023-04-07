164d1d552d9c0dc5b9c7ffc4787b10e61a2f6dd4
YegorMerkulov <116704193+YegorMerkulov@users.noreply.github.com>
2023-04-07 19:06:18 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,9 +1,9 @@
 // Copyright 2021 NNTU-CS
 
 int cbinsearch(int *arr, int size, int value) {
-  int count=0;
-  for(int i=0; i<size;i++){
-    if(arr[i]==value)
+  int count = 0;
+  for (int i = 0; i < size; i++) {
+    if (arr[i] == value)
       count+=1;
   }
   return count;
